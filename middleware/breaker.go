3d66cedbf2e3458d6d33cad88dad0a489ec84b33
jagath <jagath.bandara@pickme.lk>
2024-02-29 00:47:21 +0530

    added the configurations to specyfy the blockage type, success status list and the mar allowed continues error count

--- middleware/breaker.go
+++ middleware/breaker.go
@@ -6,65 +6,150 @@ import (
 	"time"
 )
 
+func InitBreakerConfig(numAllowedErrorResp int, successStatus []httpStatus, retryDurationAfterBlocked []time.Duration, blockLevel blockLevel) *breakerConfig {
+	if numAllowedErrorResp <= 0 {
+		numAllowedErrorResp = 3
+	}
+	tailArr = make([]bool, breakerConf.NumAllowedErrorResp)
+	for i := range tailArr {
+		tailArr[i] = true
+	}
+	if successStatus == nil || len(successStatus) == 0 {
+		var successStatus []httpStatus
+		successStatus[0] = StatusOK
+	}
+	if retryDurationAfterBlocked == nil || len(retryDurationAfterBlocked) == 0 {
+		var allowMillsAfterWhenBlocked []time.Duration
+		allowMillsAfterWhenBlocked[0] = time.Second
+	}
+	if blockLevel != BlockLevelEndpoint && blockLevel != BlockLevelRouter {
+		blockLevel = BlockLevelEndpoint
+	}
+	return &breakerConfig{
+		NumAllowedErrorResp:       numAllowedErrorResp,
+		SuccessStatus:             successStatus,
+		RetryDurationAfterBlocked: retryDurationAfterBlocked,
+		BlockLevel:                blockLevel,
+	}
+}
+
 func CircuitBreaker(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Request URL: %s", r.URL)
-		prevCalls, ok := endpointMap.Load(r.URL.String())
-		crw := &customResponseWriter{ResponseWriter: w}
-		if !ok {
-			newCallInfo := ReqInfo{
-				uRL:            r.URL.String(),
-				tail:           [3]bool{true, true, true},
-				isBlocked:      false,
-				blockTimeLevel: 0,
-				blockType:      1,
-				lastPing:       time.Now(),
-			}
-			endpointMap.Store(r.URL.String(), newCallInfo)
-			next.ServeHTTP(crw, r)
-		} else {
-			info := prevCalls.(ReqInfo)
-			if info.isBlocked {
-				if time.Since(info.lastPing) > 3*time.Second {
+		if breakerConf.BlockLevel == BlockLevelEndpoint {
+			prevCalls, ok := endpointMap.Load(r.URL.String())
+
+			crw := &customResponseWriter{ResponseWriter: w}
+			if !ok {
+				newCallInfo := ReqInfo{
+					uRL:            r.URL.String(),
+					tail:           tailArr,
+					isBlocked:      false,
+					blockTimeLevel: 0,
+					lastPing:       time.Now(),
+				}
+
+				endpointMap.Store(r.URL.String(), newCallInfo)
+				next.ServeHTTP(crw, r)
+			} else {
+				info := prevCalls.(ReqInfo)
+				if info.isBlocked {
+					if time.Since(info.lastPing) > 3*time.Second {
+						info.lastPing = time.Now()
+						endpointMap.Store(r.URL.String(), info)
+					} else {
+						http.Error(w, "Forbidden", http.StatusForbidden)
+						return
+					}
+				} else {
 					info.lastPing = time.Now()
 					endpointMap.Store(r.URL.String(), info)
-				} else {
-					http.Error(w, "Forbidden", http.StatusForbidden)
-					return
+				}
+			}
+			next.ServeHTTP(crw, r)
+
+			log.Printf("Response status: %d", crw.status)
+			prevCalls, ok = endpointMap.Load(r.URL.String())
+			info := prevCalls.(ReqInfo)
+			lastIndex := len(info.tail) - 1
+			if breakerConf.containsState(httpStatus(crw.status)) {
+				for i := 0; i <= lastIndex; i++ {
+					info.tail[i] = true
 				}
 			} else {
-				info.lastPing = time.Now()
-				endpointMap.Store(r.URL.String(), info)
+				j := 0
+				for i := 0; i <= lastIndex; i++ {
+					if i < lastIndex {
+						info.tail[i] = info.tail[i+1]
+					} else {
+						info.tail[i] = false
+					}
+					if !info.tail[i] {
+						j++
+					}
+				}
+				if j > lastIndex {
+					info.isBlocked = true
+				}
 			}
-		}
-
-		next.ServeHTTP(crw, r)
-
-		// Intercept the response here
-		log.Printf("Response status: %d", crw.status)
-		prevCalls, ok = endpointMap.Load(r.URL.String())
-		info := prevCalls.(ReqInfo)
-		lastIndex := len(info.tail) - 1
-		if crw.status == http.StatusOK || crw.status == http.StatusCreated {
-			for i := 0; i <= lastIndex; i++ {
-				info.tail[i] = true
-			}
+			endpointMap.Store(r.URL.String(), info)
 		} else {
-			j := 0
-			for i := 0; i <= lastIndex; i++ {
-				if i < lastIndex {
-					info.tail[i] = info.tail[i+1]
-				} else {
-					info.tail[i] = false
+			crw := &customResponseWriter{ResponseWriter: w}
+			if RouterReqStatObj.lastPing.IsZero() {
+				RouterReqStatObj = RouterReqStat{
+					tail:           []bool{},
+					isBlocked:      false,
+					blockTimeLevel: 0,
+					lastPing:       time.Now(),
 				}
-				if !info.tail[i] {
-					j++
+				next.ServeHTTP(crw, r)
+			} else {
+
+				if RouterReqStatObj.isBlocked {
+					if time.Since(RouterReqStatObj.lastPing) > 3*time.Second {
+						RouterReqStatObj.lastPing = time.Now()
+					} else {
+						http.Error(w, "Forbidden", http.StatusForbidden)
+						return
+					}
+				} else {
+					RouterReqStatObj.lastPing = time.Now()
 				}
 			}
-			if j > lastIndex {
-				info.isBlocked = true
+			next.ServeHTTP(crw, r)
+
+			log.Printf("Response status: %d", crw.status)
+
+			lastIndex := len(RouterReqStatObj.tail) - 1
+			if breakerConf.containsState(httpStatus(crw.status)) {
+				for i := 0; i <= lastIndex; i++ {
+					RouterReqStatObj.tail[i] = true
+				}
+			} else {
+				j := 0
+				for i := 0; i <= lastIndex; i++ {
+					if i < lastIndex {
+						RouterReqStatObj.tail[i] = RouterReqStatObj.tail[i+1]
+					} else {
+						RouterReqStatObj.tail[i] = false
+					}
+					if !RouterReqStatObj.tail[i] {
+						j++
+					}
+				}
+				if j > lastIndex {
+					RouterReqStatObj.isBlocked = true
+				}
 			}
 		}
-		endpointMap.Store(r.URL.String(), info)
 	})
+}
+
+func (s breakerConfig) containsState(status httpStatus) bool {
+	for _, code := range s.SuccessStatus {
+		if code == status {
+			return true
+		}
+	}
+	return false
 }
--- middleware/model.go
+++ middleware/model.go
@@ -9,14 +9,119 @@ import (
 	"time"
 )
 
+/*
+init to
+
+give the consecutive error number
+give the success status types
+give the retry time series.
+give is it for endpoint wise or total endpoints
+*/
+
+type blockLevel int
+
+const (
+	BlockLevelEndpoint blockLevel = 1
+	BlockLevelRouter   blockLevel = 2
+)
+
+type httpStatus int
+
+const (
+	StatusSwitchingProtocols httpStatus = 101 // RFC 9110, 15.2.2
+	StatusContinue           httpStatus = 100 // RFC 9110, 15.2.1
+	StatusProcessing         httpStatus = 102 // RFC 2518, 10.1
+	StatusEarlyHints         httpStatus = 103 // RFC 8297
+
+	StatusOK                   httpStatus = 200 // RFC 9110, 15.3.1
+	StatusCreated              httpStatus = 201 // RFC 9110, 15.3.2
+	StatusAccepted             httpStatus = 202 // RFC 9110, 15.3.3
+	StatusNonAuthoritativeInfo httpStatus = 203 // RFC 9110, 15.3.4
+	StatusNoContent            httpStatus = 204 // RFC 9110, 15.3.5
+	StatusResetContent         httpStatus = 205 // RFC 9110, 15.3.6
+	StatusPartialContent       httpStatus = 206 // RFC 9110, 15.3.7
+	StatusMultiStatus          httpStatus = 207 // RFC 4918, 11.1
+	StatusAlreadyReported      httpStatus = 208 // RFC 5842, 7.1
+	StatusIMUsed               httpStatus = 226 // RFC 3229, 10.4.1
+
+	StatusMultipleChoices   httpStatus = 300 // RFC 9110, 15.4.1
+	StatusMovedPermanently  httpStatus = 301 // RFC 9110, 15.4.2
+	StatusFound             httpStatus = 302 // RFC 9110, 15.4.3
+	StatusSeeOther          httpStatus = 303 // RFC 9110, 15.4.4
+	StatusNotModified       httpStatus = 304 // RFC 9110, 15.4.5
+	StatusUseProxy          httpStatus = 305 // RFC 9110, 15.4.6
+	_                       httpStatus = 306 // RFC 9110, 15.4.7 (Unused)
+	StatusTemporaryRedirect httpStatus = 307 // RFC 9110, 15.4.8
+	StatusPermanentRedirect httpStatus = 308 // RFC 9110, 15.4.9
+
+	StatusBadRequest                             httpStatus = 400 // RFC 9110, 15.5.1
+	StatusUnauthorized                           httpStatus = 401 // RFC 9110, 15.5.2
+	StatusPaymentRequired                        httpStatus = 402 // RFC 9110, 15.5.3
+	StatusForbidden                              httpStatus = 403 // RFC 9110, 15.5.4
+	StatusNotFound                               httpStatus = 404 // RFC 9110, 15.5.5
+	StatusMethodNotAllowed                       httpStatus = 405 // RFC 9110, 15.5.6
+	StatusNotAcceptable                          httpStatus = 406 // RFC 9110, 15.5.7
+	StatusProxyAuthRequired                      httpStatus = 407 // RFC 9110, 15.5.8
+	StatusRequestTimeout                         httpStatus = 408 // RFC 9110, 15.5.9
+	StatusConflict                               httpStatus = 409 // RFC 9110, 15.5.10
+	StatusGone                                   httpStatus = 410 // RFC 9110, 15.5.11
+	StatusLengthRequired                         httpStatus = 411 // RFC 9110, 15.5.12
+	StatusPreconditionFailed                     httpStatus = 412 // RFC 9110, 15.5.13
+	StatusRequestEntityTooLarge                  httpStatus = 413 // RFC 9110, 15.5.14
+	StatusRequestURITooLong                      httpStatus = 414 // RFC 9110, 15.5.15
+	StatusUnsupportedMediaType                   httpStatus = 415 // RFC 9110, 15.5.16
+	StatusRequestedRangeNotSatisfiablehttpStatus            = 416 // RFC 9110, 15.5.17
+	StatusExpectationFailed                      httpStatus = 417 // RFC 9110, 15.5.18
+	StatusTeapot                                 httpStatus = 418 // RFC 9110, 15.5.19 (Unused)
+	StatusMisdirectedRequest                     httpStatus = 421 // RFC 9110, 15.5.20
+	StatusUnprocessableEntity                    httpStatus = 422 // RFC 9110, 15.5.21
+	StatusLocked                                 httpStatus = 423 // RFC 4918, 11.3
+	StatusFailedDependency                       httpStatus = 424 // RFC 4918, 11.4
+	StatusTooEarly                               httpStatus = 425 // RFC 8470, 5.2.
+	StatusUpgradeRequired                        httpStatus = 426 // RFC 9110, 15.5.22
+	StatusPreconditionRequired                   httpStatus = 428 // RFC 6585, 3
+	StatusTooManyRequests                        httpStatus = 429 // RFC 6585, 4
+	StatusRequestHeaderFieldsTooLarge            httpStatus = 431 // RFC 6585, 5
+	StatusUnavailableForLegalReasons             httpStatus = 451 // RFC 7725, 3
+
+	StatusInternalServerError           httpStatus = 500 // RFC 9110, 15.6.1
+	StatusNotImplemented                httpStatus = 501 // RFC 9110, 15.6.2
+	StatusBadGateway                    httpStatus = 502 // RFC 9110, 15.6.3
+	StatusServiceUnavailable            httpStatus = 503 // RFC 9110, 15.6.4
+	StatusGatewayTimeout                httpStatus = 504 // RFC 9110, 15.6.5
+	StatusHTTPVersionNotSupported       httpStatus = 505 // RFC 9110, 15.6.6
+	StatusVariantAlsoNegotiates         httpStatus = 506 // RFC 2295, 8.1
+	StatusInsufficientStorage           httpStatus = 507 // RFC 4918, 11.5
+	StatusLoopDetected                  httpStatus = 508 // RFC 5842, 7.2
+	StatusNotExtended                   httpStatus = 510 // RFC 2774, 7
+	StatusNetworkAuthenticationRequired httpStatus = 511 // RFC 6585, 6
+
+)
+
+type breakerConfig struct {
+	NumAllowedErrorResp       int //number of consequent error responses allowed before blocking the requests
+	SuccessStatus             []httpStatus
+	RetryDurationAfterBlocked []time.Duration
+	BlockLevel                blockLevel
+}
+
+var breakerConf breakerConfig
 var endpointMap sync.Map
+var tailArr []bool
+var RouterReqStatObj RouterReqStat
+
+type RouterReqStat struct {
+	tail           []bool
+	isBlocked      bool
+	blockTimeLevel int
+	lastPing       time.Time
+}
 
 type ReqInfo struct {
 	uRL            string
-	tail           [3]bool
+	tail           []bool
 	isBlocked      bool
 	blockTimeLevel int
-	blockType      int
 	lastPing       time.Time
 }
 
